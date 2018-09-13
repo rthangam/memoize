84d69c97b60800c1281d6e12af2181e26af0bd92
Ramesh Thangamani <ramesh.thangamani@oracle.com>
2018-09-13 22:15:52 +0530

    New File
    Memoize with simple cache

---
+++ memoizeV1.js
@@ -0,0 +1,29 @@
+function memoize(param){
+	if(!memoize.cache){
+		memoize.cache = {};
+	}
+
+	if(!memoize.cache[param]){
+		memoize.cache[param] = fib(param);
+	}
+
+	return memoize.cache[param];
+}
+
+
+
+//Fibonacci series
+function fib(n){
+  return (n < 2)? 1: fib(n-1) + fib(n - 2);  
+}
+
+
+console.log("non-memoize result");
+console.time("non-memoize");
+console.log(memoize(20));
+console.timeEnd("non-memoize");
+
+console.log("memoize result");
+console.time("memoize");
+console.log(memoize(20));
+console.timeEnd("memoize");
